19d7749a45dd54369cdb0eeb6335fc7074197cd3
Celine Ly <celinely1999@gmail.com>
2019-01-19 14:32:21 -0500

    added weatherAPI.js

---
+++ weatherAPI.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    
+    function checkWeather() {
+        var URL = 'https://fcc-weather-api.glitch.me/api/current?lat=45.504640&lon=-73.612894';
+        
+        $.getJSON(URL, function(data){
+            console.log(data);
+            updateWeather(data);
+        });
+    }
+    checkWeather();
+    
+    
+    function updateWeather(data){
+        var city = data.name;
+        
+        $('#city').html(city);
+    }
+});
