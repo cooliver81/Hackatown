b9f5c20d73fb9ecf61330c08dceba7bc74943a5e
Oliver Jean <ojean128@gmail.com>
2019-01-19 17:00:41 -0500

    weather is done

--- weatherAPI.js
+++ weatherAPI.js
@@ -1,18 +1,13 @@
 $(document).ready(function() {
-    
     //Location
     // Check whether browser supports Geolocation API or not
     if (navigator.geolocation) // Supported
     {
-       navigator.geolocation.getCurrentPosition(success, error);
+       navigator.geolocation.getCurrentPosition(success);
     }
     else
     {
-      alert("Oops! This browser does not support HTML Geolocation.");
-    }
-    
-    function getLocation() {
-        var URL = ''
+      error();
     }
     
     function success(pos) {
@@ -22,27 +17,29 @@ $(document).ready(function() {
     }
     
     function error() {
-        console.log('Error')
+        console.log('Error');
+        alert("Oops! This browser does not support HTML Geolocation.");
     }
     
     
     //Weather API
     function checkWeather(lat, long) {
-        var URL = 'https://fcc-weather-api.glitch.me/api/current?lat=${lat}&lon=${long}';
+        var URL = `https://fcc-weather-api.glitch.me/api/current?lat=${lat}&lon=${long}`;
         
         $.getJSON(URL, function(data){
             console.log(data);
             updateWeather(data);
         });
     }
-    checkWeather();
-    
     
     function updateWeather(data){
         var city = data.name;
         var temp = data.main.temp;
+        var description = data.weather[0].description;
         
         $('#city').html(city);
         $('#temp').html("The weather is " + temp);
+        $('#description').html(description);
     }
 });
+
